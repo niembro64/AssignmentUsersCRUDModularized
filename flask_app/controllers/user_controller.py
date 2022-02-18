5d18dbfb76424bb13ed36da657850f445cdc380c
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-17 20:57:29 -0600

    Populating Edit FIelds

--- flask_app/controllers/user_controller.py
+++ flask_app/controllers/user_controller.py
@@ -11,17 +11,11 @@ def index():
     users = User.all_users()
     return render_template("index.html", all_users=users)
 
-
 @app.route("/users")
 def show_all_users():
     # call the get all classmethod to get all users
     users = User.all_users()
     return render_template("users.html", all_users=users)
-
-# ====================================
-# Show One User Route
-# ====================================
-
 
 @app.route("/users/<int:user_id>")
 def show_user(user_id):
@@ -31,15 +25,19 @@ def show_user(user_id):
     user = User.one_user(data)
     return render_template("show_one.html", one_user=user)
 
-# ====================================
-# Add User Routes
-# ====================================
-
-
 @app.route("/new_user")
 def new_user():
     return render_template("new_user.html")
 
+@app.route("/users/<int:user_id>/edit")
+def edit_user(user_id):
+    data = {
+        "id": user_id
+    }
+    user = User.one_user(data)
+    return render_template("edit_user.html", one_user=user)
+
+###############################
 
 @app.route("/create_user", methods=["POST"])
 def create_user():
@@ -51,7 +49,6 @@ def create_user():
     user_id = User.save_user(data)
     return redirect(f"/users/{user_id}")
 
-
 @app.route("/users/<int:user_num>/delete", methods=["POST"])
 def delete_user(user_num):
     data = {
@@ -59,3 +56,4 @@ def delete_user(user_num):
         }
     User.delete_user(data)
     return redirect("/users")
+
